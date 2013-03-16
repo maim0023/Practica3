7ca4c803467542a15bf92f137890fa21d03f1329
maim0023 <maim0023@outlook.com>
2013-03-16 17:44:32 -0600

    Create Persona.cs
    clase persona

---
+++ Persona.cs
@@ -0,0 +1,20 @@
+using System;
+namespace practica3
+{
+  public class Persona
+	{
+		public string codigo;
+		public string nombre;
+		public string direccion;
+		public string correo;
+		public string telefono;
+		
+		public Persona(){
+			this.codigo = "";
+			this.nombre = "";
+			this.direccion = "";
+			this.correo = "";
+			this.telefono = "";
+		}
+	}
+}
